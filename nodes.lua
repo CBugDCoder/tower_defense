614864276affc2f8da549ac340c822a46adfaf26
CBugDCoder <cbugdcoder315@gmail.com>
2020-04-10 15:14:55 -0500

    Update visuals

--- nodes.lua
+++ nodes.lua
@@ -1,5 +1,3 @@
---tower_defense/nodes.lua 
-
 ---------------
 --Unbreakable--
 ---------------
@@ -30,7 +28,24 @@ minetest.register_node("tower_defense:barrier", {
 --Flag
 minetest.register_node("tower_defense:flag", {
 	tiles = {"default_dirt.png"},
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-5/16,-8/16,-1/16,-3/16,8/16,1/16},
+			{-3/16,1/16,-1/16,6/16,7/16,1/16},
+		}
+	},
+	tiles = {
+		"tower_defense_flag_top.png^[transform1",
+		"tower_defense_flag_top.png^[transform1",
+		"tower_defense_flag_front.png",
+		"tower_defense_flag_back.png",
+		"tower_defense_flag_side.png^[transform4",
+		"tower_defense_flag_side.png",
+	},
 	description = "Flag",
+	paramtype = "light",
 	groups = {not_in_creative_inventory = 1, unbreakable = 1, health = 1000}
 })
 
@@ -47,19 +62,19 @@ minetest.register_node("tower_defense:wooden_barricade",{
 
 minetest.register_node("tower_defense:stone_barricade",{
 	description = "Stone Barricade",
-	tiles = {"default_wood.png"},
+	tiles = {"default_stone.png"},
 	groups = {not_in_creative_inventory = 1, oddly_breakable_by_hand = 3, health = 100}
 })
 
 minetest.register_node("tower_defense:steel_barricade",{
 	description = "Steel Barricade",
-	tiles = {"default_wood.png"},
+	tiles = {"default_steel_block.png"},
 	groups = {not_in_creative_inventory = 1, oddly_breakable_by_hand = 3, health = 250}
 })
 
 minetest.register_node("tower_defense:obsidian_barricade",{
 	description = "Obsidian Barricade",
-	tiles = {"default_wood.png"},
+	tiles = {"default_obsidian.png"},
 	groups = {not_in_creative_inventory = 1, oddly_breakable_by_hand = 3, health = 500}
 })
 
